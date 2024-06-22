8f0f80023eba599dcbc2ee799cfdc48ee327e18f
Sage Walker <sagew@science.xyz>
2024-06-21 18:13:43 -0700

    Update api pin

--- synapsectl/src/synapsectl/synapse_ctl.py
+++ synapsectl/src/synapsectl/synapse_ctl.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 import grpc
 from google.protobuf.empty_pb2 import Empty
-import generated.synapse_pb2_grpc as synapse_pb2_grpc
+from generated.api.synapse_pb2_grpc import SynapseDeviceStub
+from generated.api.synapse_pb2 import DeviceInfo
+# from generated.node_pb2 import NodeOptions
 
 def main():
     with grpc.insecure_channel('0.0.0.0:50051') as channel:
-        stub = synapse_pb2_grpc.SynapseDeviceStub(channel)
+        stub = SynapseDeviceStub(channel)
         response = stub.Info(Empty())
+
         print(response)
 
 if __name__ == '__main__':
