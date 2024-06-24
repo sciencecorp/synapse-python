ce92537a7f44c0f5b723a2b33ba333948033b53c
Max Hodak <maxhodak@gmail.com>
2024-06-23 20:47:39 -0700

    matching updated synpase interface

--- synapse/device.py
+++ synapse/device.py
@@ -1,58 +1,59 @@
 import grpc
 from google.protobuf import text_format
 from google.protobuf.empty_pb2 import Empty
-from generated.api.synapse_pb2 import SynapseStatusCode
+from generated.api.synapse_pb2 import StatusCode
 from generated.api.synapse_pb2_grpc import SynapseDeviceStub
 
+
 class Device(object):
-  sockets = None
+    sockets = None
 
-  def __init__(self, uri):
-    self.uri = uri
+    def __init__(self, uri):
+        self.uri = uri
 
-    self.channel = grpc.insecure_channel(uri)
-    self.rpc = SynapseDeviceStub(self.channel)
+        self.channel = grpc.insecure_channel(uri)
+        self.rpc = SynapseDeviceStub(self.channel)
 
-  def start(self):
-    try:
-      response = self.rpc.Start(Empty())
-      if self._handle_status_response(response):
-        return response
-    except grpc.RpcError as e:
-      print("Error: ", e.details())
-    return False
+    def start(self):
+        try:
+            response = self.rpc.Start(Empty())
+            if self._handle_status_response(response):
+                return response
+        except grpc.RpcError as e:
+            print("Error: ", e.details())
+        return False
 
-  def stop(self):
-    try:
-      response = self.rpc.Stop(Empty())
-      if self._handle_status_response(response):
-        return response
-    except grpc.RpcError as e:
-      print("Error: ", e.details())
-    return False
+    def stop(self):
+        try:
+            response = self.rpc.Stop(Empty())
+            if self._handle_status_response(response):
+                return response
+        except grpc.RpcError as e:
+            print("Error: ", e.details())
+        return False
 
-  def info(self):
-    try:
-      response = self.rpc.Info(Empty())
-      return response
-    except grpc.RpcError as e:
-      print("Error: ", e.details())
-      return None
+    def info(self):
+        try:
+            response = self.rpc.Info(Empty())
+            return response
+        except grpc.RpcError as e:
+            print("Error: ", e.details())
+            return None
 
-  def configure(self, config):
-    config.set_device(self)
-    try:
-      response = self.rpc.Configure(config.to_proto())
-      if self._handle_status_response(response):
-        return response
-    except grpc.RpcError as e:
-      print("Error: ", e.details())
-    return False
-  
-  def _handle_status_response(self, status):
-    if status.code != SynapseStatusCode.kOk:
-      print("Error %d: %s" % (status.code, status.message))
-      return False
-    else:
-      self.sockets = status.sockets
-      return True
+    def configure(self, config):
+        config.set_device(self)
+        try:
+            response = self.rpc.Configure(config.to_proto())
+            if self._handle_status_response(response):
+                return response
+        except grpc.RpcError as e:
+            print("Error: ", e.details())
+        return False
+
+    def _handle_status_response(self, status):
+        if status.code != StatusCode.kOk:
+            print("Error %d: %s" % (status.code, status.message))
+            return False
+        else:
+            self.sockets = status.sockets
+            return True
