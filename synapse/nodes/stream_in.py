2af690821aa4da07ca158e11b5d9f59596a2c58b
Max Hodak <maxhodak@gmail.com>
2024-06-23 23:48:52 -0700

    write to streamins

--- synapse/nodes/stream_in.py
+++ synapse/nodes/stream_in.py
@@ -1,3 +1,4 @@
+import zmq
 from synapse import ChannelMask
 from synapse.node import Node
 from synapse.generated.api.node_pb2 import NodeConfig, NodeType
@@ -6,11 +7,23 @@ from synapse.generated.api.nodes.stream_in_pb2 import StreamInConfig
 
 class StreamIn(Node):
     def __init__(self):
-        pass
+        ctx = zmq.Context.instance()
+        self.socket = zmq.Socket(ctx, zmq.PUB)
 
-    def write(self):
+    def write(self, data):
         if self.device is None:
             return False
+
+        socket = next((s for s in self.device.sockets if s.node_id == self.id), None)
+
+        if socket is None:
+            return False
+
+        self.socket.connect(socket.bind)
+        try:
+            self.socket.send_string(data)
+        except zmq.ZMQError as e:
+            print(f"Error sending data: {e}")
         return True
 
     def to_proto(self):
