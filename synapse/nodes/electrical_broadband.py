9a549a956312951b90df3555ecdc613a2993138a
Sage Walker <sagew@science.xyz>
2024-06-28 21:28:17 -0700

    Small fix to default to emty channel mask

--- synapse/nodes/electrical_broadband.py
+++ synapse/nodes/electrical_broadband.py
@@ -6,7 +6,7 @@ from synapse.api.api.nodes.electrical_broadband_pb2 import (
 
 
 class ElectricalBroadband(Node):
-    def __init__(self, channel_mask):
+    def __init__(self, channel_mask=None):
         self.channel_mask = channel_mask
 
     def to_proto(self):
@@ -16,8 +16,8 @@ class ElectricalBroadband(Node):
 
         p = ElectricalBroadbandConfig()
         p.peripheral_id = 0
-        for i in self.channel_mask.iter_channels():
-            p.ch_mask.append(i)
+        # for i in self.channel_mask.iter_channels():
+        #     p.ch_mask.append(i)
         p.bit_width = 10
         p.sample_rate = 20000
         p.gain = 1
