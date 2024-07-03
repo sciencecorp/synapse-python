c02607a0782222571a099c1bb89c397c35b69469
Antonia Elsen <antoniaelsen@gmail.com>
2024-07-02 17:46:52 -0700

    feat: convert StreamOut node to UDP multicast

--- synapse/cli/streaming.py
+++ synapse/cli/streaming.py
@@ -1,70 +1,86 @@
+from synapse.api.api.node_pb2 import NodeType
 from synapse.device import Device
 from synapse.config import Config
 from synapse.nodes.electrical_broadband import ElectricalBroadband
 from synapse.nodes.stream_out import StreamOut
-from synapse.generated.api.node_pb2 import NodeType
 
 def add_commands(subparsers):
-    a = subparsers.add_parser("read", help="Read from a device StreamOut Node")
+    a = subparsers.add_parser("read", help="Read from a device's StreamOut Node")
     a.add_argument("uri", type=str)
     a.add_argument("node_id", type=int)
+    a.add_argument("-o", "--out", type=str, help="Output file")
     a.set_defaults(func=read)
 
-    a = subparsers.add_parser("write", help="Write to a device StreamIn Node")
+    a = subparsers.add_parser("write", help="Write to a device's StreamIn Node")
     a.add_argument("uri", type=str)
     a.add_argument("node_id", type=int)
+    a.add_argument("-i", "--in", type=str, help="Input file")
     a.set_defaults(func=write)
 
 
 def read(args):
-
-    config = Config()
-    stream_out = StreamOut()
-    ephys = ElectricalBroadband()
-    config.add_node(stream_out)
-    config.add_node(ephys)
-    config.connect(ephys, stream_out)
-
     dev = Device(args.uri)
-    st = dev.configure(config)
-    if not st:
-        print("Failed to configure device")
-        return
 
+    print("Reading from device...")
     info = dev.info()
     if info is None:
         print("Couldnt get device info")
         return
-    print(info)
-    nodes = info.configuration.nodes
-    out_node = None
-    for node in nodes:
-        if (node.id == args.node_id):
-            if node.type != NodeType.kStreamOut:
-                print(f"Node ID {args.node_id} is not a StreamOut node")
-                return
-            out_node = node
-            break
 
-    if out_node is None:
+    print(info)
+
+    nodes = info.configuration.nodes
+    out_node = [node for node in nodes if node.id == args.node_id and node.type == NodeType.kStreamOut]
+    if not len(out_node):
         print(f"Node ID {args.node_id} not found in device's signal chain")
         return
 
-    from io import BytesIO
+    config = Config()
+    stream_out = StreamOut()
+    ephys = ElectricalBroadband()
+
+    config.add_node(stream_out)
+    config.add_node(ephys)
+    config.connect(ephys, stream_out)
+
+    print("Configuring device...")
+ 
+    if not dev.configure(config):
+        print("Failed to configure device")
+        return
+    print(" - done.")
+
+    print("Starting device...")
     if not dev.start():
         print("Failed to start device")
         return
-    with open("test.txt", "wb") as f:
-        for i in range(250):
-            data = stream_out.read()
-            # wb = BytesIO(data[0])
-            # for d in data:
+    print(" - done.")
 
-                # print(type(d))
-            f.writelines(data)
+    print("Streaming data... press Ctrl+C to stop")
+    if args.out:
+        with open(args.out, "wb") as f:
+            try:
+                while True:
+                    data = stream_out.read()
+                    if data is not None:
+                        f.write(data)
+            except KeyboardInterrupt:
+                pass
+    else:
+        try:
+            while True:
+                data = stream_out.read()
+                if data is not None:
+                    print(data)
+        except KeyboardInterrupt:
+            pass
 
-    print("Stopping device")
-    dev.stop()
+    print("Stopping device...")
+    if not dev.stop():
+        print("Failed to stop device")
+        return
+    print(" - done.")
+
 
 def write(args):
     pass
--- synapse/nodes/stream_in.py
+++ synapse/nodes/stream_in.py
@@ -1,4 +1,3 @@
-import zmq
 from synapse import ChannelMask
 from synapse.node import Node
 from synapse.api.api.node_pb2 import NodeConfig, NodeType
@@ -7,8 +6,9 @@ from synapse.api.api.nodes.stream_in_pb2 import StreamInConfig
 
 class StreamIn(Node):
     def __init__(self):
-        ctx = zmq.Context.instance()
-        self.socket = zmq.Socket(ctx, zmq.PUB)
+        # ctx = zmq.Context.instance()
+        # self.socket = zmq.Socket(ctx, zmq.PUB)
+        pass
 
     def write(self, data):
         if self.device is None:
@@ -19,10 +19,11 @@ class StreamIn(Node):
         if socket is None:
             return False
 
-        self.socket.connect(socket.bind)
+        # self.socket.connect(socket.bind)
         try:
-            self.socket.send_string(data)
-        except zmq.ZMQError as e:
+            # self.socket.send_string(data)
+            pass
+        except Exception as e:
             print(f"Error sending data: {e}")
         return True
 
