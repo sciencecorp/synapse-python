32dd6733ab2f2a3d62028b7d685bc951fdfadf9e
Antonia Elsen <antoniaelsen@gmail.com>
2024-07-03 16:13:47 -0700

    chore: make multicast an option on StreamIn, StreamOut nodes; default unicast

--- synapse/nodes/stream_in.py
+++ synapse/nodes/stream_in.py
@@ -1,6 +1,5 @@
 import socket
-import struct
-from synapse import ChannelMask
+import time
 from synapse.node import Node
 from synapse.api.api.node_pb2 import NodeConfig, NodeType
 from synapse.api.api.nodes.stream_in_pb2 import StreamInConfig
@@ -8,10 +7,10 @@ from synapse.api.api.nodes.stream_in_pb2 import StreamInConfig
 MULTICAST_TTL = 3
 
 class StreamIn(Node):
-    def __init__(self):
+    def __init__(self, multicast_group=None):
         self.__socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.__socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
-        pass
+        self.__multicast_group = multicast_group
 
     def write(self, data):
         if self.device is None:
@@ -27,6 +26,8 @@ class StreamIn(Node):
 
         try:
             self.__socket.sendto(data, (group, port))
+            # https://stackoverflow.com/questions/21973661/os-x-udp-send-error-55-no-buffer-space-available
+            time.sleep(0.00001)
         except Exception as e:
             print(f"Error sending data: {e}")
         return True
@@ -36,9 +37,11 @@ class StreamIn(Node):
         n.type = NodeType.kStreamIn
         n.id = self.id
 
-        o = StreamInConfig()
-        o.shape.append(2048)
-        o.shape.append(1)
+        i = StreamInConfig()
+        i.shape.append(2048)
+        i.shape.append(1)
+        if self.__multicast_group:
+            i.multicast_group = self.__multicast_group
 
-        n.stream_in.CopyFrom(o)
+        n.stream_in.CopyFrom(i)
         return n
