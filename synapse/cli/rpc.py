309ceb3d9a0a2019ed2ee26c90708661b718b261
Sage Walker <sagew@science.xyz>
2024-07-06 13:59:45 -0700

    configure reads config from json

--- synapse/cli/rpc.py
+++ synapse/cli/rpc.py
@@ -1,6 +1,8 @@
+from pathlib import Path
 from synapse.device import Device
+from synapse.api.api.synapse_pb2 import DeviceConfiguration
 from google.protobuf import text_format
-
+from google.protobuf.json_format import Parse
 
 def add_commands(subparsers):
     a = subparsers.add_parser("info", help="Get device information")
@@ -17,6 +19,7 @@ def add_commands(subparsers):
 
     a = subparsers.add_parser("configure", help="Write a configuration to the device")
     a.add_argument("uri", type=str)
+    a.add_argument("config_file", type=str)
     a.set_defaults(func=configure)
 
 
@@ -35,4 +38,11 @@ def stop(args):
 
 
 def configure(args):
-    return Device(args.uri).configure()
+    if Path(args.config_file).suffix != ".json":
+        print("Configuration file must be a JSON file")
+        return False
+    
+    with open(args.config_file) as config_json:
+        config_proto = Parse(config_json.read(), DeviceConfiguration())
+        print(config_proto)
+        return Device(args.uri).configure(config_proto)
