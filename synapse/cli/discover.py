c92134b4812ffa179597944eaa9f11cc52f27da2
Antonia Elsen <antoniaelsen@gmail.com>
2024-09-09 16:43:40 -0700

    chore: fix discover util, use in CLI discover cmd

--- synapse/cli/discover.py
+++ synapse/cli/discover.py
@@ -1,6 +1,4 @@
-import time
-import socket
-import struct
+from synapse.utils.discover import discover as _discover
 
 BROADCAST_PORT = 6470
 BROADCAST_ADDR = "224.0.0.245"
@@ -17,40 +15,6 @@ def add_commands(subparsers):
 
 
 def discover(args):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(3)
-    ttl = struct.pack("b", 3)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-
-    devices = []
-
-    try:
-        start_time = time.time()
-
-        sent = sock.sendto(
-            "DISCOVER {}".format(args.auth_code).encode("ascii"),
-            (BROADCAST_ADDR, BROADCAST_PORT),
-        )
-        while True:
-            if time.time() - start_time > 3:
-                break
-            try:
-                data, server = sock.recvfrom(1024)
-            except socket.timeout:
-                continue
-            else:
-                data = data.decode("ascii").split()
-                if data[0] == "ID":
-                    if len(data) != 5:
-                        print("Invalid ID response from {!r}".format(server))
-                        continue
-                    command, serial, capability, port, name = data
-                    print(
-                        "{}:{}   {}   {} ({})".format(
-                            server[0], port, capability, name, serial
-                        )
-                    )
-                else:
-                    print("Unknown response: {!r}".format(command))
-    finally:
-        sock.close()
+    devices = _discover(args.auth_code)
+    for d in devices:
+      print(f"{d.host}:{d.port}   {d.capability}   {d.name} ({d.serial})")
--- synapse/utils/discover.py
+++ synapse/utils/discover.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import time
 import socket
 import struct
@@ -5,9 +6,18 @@ import struct
 BROADCAST_PORT = 6470
 BROADCAST_ADDR = "224.0.0.245"
 
-def discover(auth_code):
+@dataclass
+class DeviceInfo:
+    host: str
+    port: int
+    capability: str
+    name: str
+    serial: str
+
+
+def discover(auth_code, timeout_sec = 3):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(3)
+    sock.settimeout(timeout_sec)
     ttl = struct.pack("b", 3)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
@@ -29,15 +39,12 @@ def discover(auth_code):
                 continue
             else:
                 data = data.decode("ascii").split()
-                print(f"got: {data}")
                 if data[0] == "ID":
                     if len(data) != 5:
                         continue
                     _, serial, capability, port, name = data
-                    print(
-                        "{}:{}   {}   {} ({})".format(
-                            server[0], port, capability, name, serial
-                        )
-                    )
+                    devices.append(DeviceInfo(server[0], int(port), capability, name, serial))
     finally:
         sock.close()
+
+    return devices
