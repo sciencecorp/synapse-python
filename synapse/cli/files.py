aa3d98c475c2f3cfe8687d851e95e038a78b5545
Sage <sage@caesium.dev>
2025-03-05 14:48:07 -0800

    add file get and recursive get function

--- synapse/cli/files.py
+++ synapse/cli/files.py
@@ -1,8 +1,10 @@
 import os
 import paramiko
 import argparse
+import stat
 from rich.console import Console
 from rich.table import Table
+from rich import progress
 
 import synapse.client.sftp as sftp
 from synapse.utils.file import * 
@@ -39,6 +41,7 @@ def add_commands(subparsers: argparse._SubParsersAction):
     b.add_argument("uri", type=str)
     b.add_argument("remote_path", type=str)
     b.add_argument("--output_path", "-o", type=str, default=os.getcwd(), required=False)
+    b.add_argument("--recursive", "-r", action="store_true", help="Download directories recursively")
     add_user_arguments(b)
     b.set_defaults(func=get)
 
@@ -64,13 +67,18 @@ def ls(args):
 
 def get(args):
     console = Console()
+    # validate_output_path(args.output_path, console)
     with console.status("Connecting to Synapse device...", spinner="bouncingBall"):
         ssh, sftp_conn = sftp.connect_sftp(args.uri, args.username, args.password)
     
     if ssh is None or sftp_conn is None:
         console.print(f"[bold red]Failed to connect to {args.uri}[/bold red]")
         return
-    get_file(sftp_conn, args.remote_path, args.output_path, console)
+    
+    if args.recursive:
+        get_dir(sftp_conn, args.remote_path, args.output_path, console)
+    else:  
+        get_file(sftp_conn, args.remote_path, args.output_path, console)
 
 def rm(args):
     print(f"Removing file from {args.uri}")
@@ -103,8 +111,8 @@ def print_file_list(files: list[paramiko.SFTPAttributes], console: Console):
         
         # Determine filename style based on type
         filename = file_attr.filename
-        is_dir = hasattr(file_attr, 'st_mode') and file_attr.st_mode & 0o40000
-        is_link = hasattr(file_attr, 'st_mode') and ((file_attr.st_mode & 0o170000) == 0o120000)
+        is_dir = stat.S_ISDIR(file_attr.st_mode)
+        is_link = stat.S_ISLNK(file_attr.st_mode)
         is_executable = hasattr(file_attr, 'st_mode') and file_attr.st_mode & 0o100
         
         if is_dir:
@@ -122,14 +130,83 @@ def print_file_list(files: list[paramiko.SFTPAttributes], console: Console):
     # Print the table
     console.print(table)
 
-def get_file(sftp_conn: paramiko.SFTPClient, remote_path: str, output_path: str, console: Console):
-    file_attrs = sftp_conn.stat(remote_path)
-    is_dir = file_attrs.st_mode & 0o40000
-
-    if is_dir:
-        console.print(f"Requested path is a directory: {remote_path}")
+def validate_output_path(output_path: str, console: Console):
+    output_abs_path = os.path.abspath(output_path)
+    output_dir = os.path.dirname(output_abs_path)
+    if not os.path.exists(output_dir):
+        console.print(f"[bold red]Output directory does not exist:[/bold red] [bold blue]{output_dir}")
         return
 
-    local_path = os.path.join(output_path, os.path.basename(remote_path)) 
-    sftp_conn.get(remote_path, local_path)
+def get_dir(sftp_conn: paramiko.SFTPClient, remote_path: str, output_path: str, console: Console): 
+    try:
+        dir_attrs = sftp_conn.stat(remote_path)
+    except FileNotFoundError:
+        console.print(f"[bold red]Directory not found:[/bold red] [bold blue]{remote_path}")
+        return
+    if not stat.S_ISDIR(dir_attrs.st_mode):
+        get_file(sftp_conn, remote_path, output_path, console)
+        return
+
+    output_path = os.path.join(output_path, os.path.basename(remote_path))
+    local_dir = os.path.dirname(output_path)
+    try:
+        if local_dir != "":
+            os.makedirs(local_dir, exist_ok=True)
+    except OSError as e:
+        console.print(f"[bold red]Failed to create output directory:[/bold red] {local_dir}")
+        return
+    
+    file_list = sftp_conn.listdir(remote_path)
+    
+    for file in file_list:
+        remote_file = os.path.join(remote_path, file)
+        local_file = os.path.join(output_path, file)
+        if stat.S_ISDIR(sftp_conn.stat(remote_file).st_mode):
+            get_dir(sftp_conn, remote_file, local_file, console)
+        else:
+            get_file(sftp_conn, remote_file, local_file, console)
+
+def get_file(sftp_conn: paramiko.SFTPClient, remote_path: str, output_path: str, console: Console):
+    try:
+        is_dir = stat.S_ISDIR(sftp_conn.stat(remote_path).st_mode)
+    except FileNotFoundError:
+        console.print(f"[bold red]File not found:[/bold red] [bold blue]{remote_path}")
+        return
+    
+    if is_dir:
+        console.print(f"[bold yellow]Requested path: [/bold yellow][blue]{remote_path}[/blue][bold yellow] is a directory. Use the --recursive flag to download directories.")
+        return
+
+    local_dir = os.path.dirname(output_path)
+    try:
+        if local_dir != "":
+            os.makedirs(local_dir, exist_ok=True)
+    except OSError as e:
+        console.print(f"[bold red]Failed to create output directory:[/bold red] {local_dir}")
+        return
+
+    local_path = output_path 
+    if os.path.isdir(output_path):
+        local_path = os.path.join(output_path, os.path.basename(remote_path))
+
+    try:
+        file_st_size = sftp_conn.stat(remote_path).st_size
+        file_size = file_st_size if file_st_size is not None else 0
+
+        prog = progress.Progress(
+            progress.SpinnerColumn(),    
+            progress.TextColumn("[progress.description]{task.description}"),
+            progress.BarColumn(),  
+            progress.DownloadColumn(),
+            progress.TransferSpeedColumn(),
+            progress.TimeElapsedColumn() 
+        )
+        with prog:
+            task = prog.add_task(f"Downloading file: {remote_path}", total=file_size)
+            def update_progress(transferred: int, total: int):
+                prog.update(task, completed=transferred)
+            sftp_conn.get(remote_path, local_path, callback=update_progress)
+    except paramiko.SFTPError as e:
+        console.print(f"[bold red]Failed to download file:[/bold red] {e}")
+        return
     
