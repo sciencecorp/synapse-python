6dc37730ffb9472bc111c9d81db811d1748931c4
Antonia Elsen <antoniaelsen@gmail.com>
2025-03-14 13:18:07 -0700

    chore: rc

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     install_requires=[
         "coolname",
         "grpcio-tools",
-        "protoletariat",
+        "protoletariat", 
         "numpy >=2.0.0",
         "pyserial",
         "scipy",
