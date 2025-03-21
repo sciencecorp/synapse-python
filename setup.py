c9e56f2ee1b54477b7b7da10fdedfa4486608357
Antonia Elsen <antoniaelsen@gmail.com>
2025-03-21 10:31:50 -0700

    chore: rc

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ extensions = [
 
 setup(
     name="science-synapse",
-    version="1.0.1",
+    version="1.0.1-rc.0",
     description="Client library and CLI for the Synapse API",
     author="Science Team",
     author_email="team@science.xyz",
