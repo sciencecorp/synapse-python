8b7e2d8ff7e99ceee0406d50e04f221188fbcd1b
Antonia Elsen <antoniaelsen@gmail.com>
2025-03-21 12:10:12 -0700

    chore: tag 1.0.1rc6

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ extensions = [
 
 setup(
     name="science-synapse",
-    version="1.0.1rc5",
+    version="1.0.1rc6",
     description="Client library and CLI for the Synapse API",
     author="Science Team",
     author_email="team@science.xyz",
