68d9fe0b759a80dcb77d24d059f9801e1df3fc12
Gilbert Montague <gilm@science.xyz>
2025-05-15 15:19:34 -0700

    chore: Bump version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ extensions = [
 
 setup(
     name="science-synapse",
-    version="2.2.3-test",
+    version="2.2.3",
     description="Client library and CLI for the Synapse API",
     author="Science Team",
     author_email="team@science.xyz",
