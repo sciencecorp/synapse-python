4a9a7f1f3a66b8830750e4e0b2cd7f3238f05e8e
gilbert <gilm@science.xyz>
2025-06-27 10:08:50 -0700

    hotfix: Fix bad import (#133)

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ extensions = [
 
 setup(
     name="science-synapse",
-    version="2.2.8",
+    version="2.2.9",
     description="Client library and CLI for the Synapse API",
     author="Science Team",
     author_email="team@science.xyz",
