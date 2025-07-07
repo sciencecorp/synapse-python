24239b440cd6dc532a151676e62bdec5b83b8dc5
gilbert <gilm@science.xyz>
2025-07-07 09:50:17 -0700

    Feature: Support updated hdf5 layout (#139)
    * chore: bump version
    
    * Feature: Support updated hdf5 layout
    
    * bump version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ extensions = [
 
 setup(
     name="science-synapse",
-    version="2.2.10",
+    version="2.2.12",
     description="Client library and CLI for the Synapse API",
     author="Science Team",
     author_email="team@science.xyz",
