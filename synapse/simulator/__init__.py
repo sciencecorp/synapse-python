5fde112ab5b7024069af7eeccdad51bb959905ed
arminvoid <3612498+arminvoid@users.noreply.github.com>
2025-08-12 18:02:08 +0100

    remove stream_in and stream_out nodes (#151)
    * remove stream_in/out nodes
    
    * remove all other references
    
    * remove reference to kStreamIn

--- synapse/simulator/__init__.py
+++ synapse/simulator/__init__.py
@@ -4,22 +4,19 @@ from synapse.simulator.nodes.broadband_source import BroadbandSource
 from synapse.simulator.nodes.optical_stimulation import OpticalStimulation
 from synapse.simulator.nodes.spike_source import SpikeSource
 from synapse.server.nodes.spectral_filter import SpectralFilter
-from synapse.server.nodes.stream_in import StreamIn
-from synapse.server.nodes.stream_out import StreamOut
 
 SIMULATOR_NODE_OBJECT_MAP = {
-  NodeType.kBroadbandSource: BroadbandSource,
-  NodeType.kSpectralFilter: SpectralFilter,
-  NodeType.kSpikeSource: SpikeSource,
-  NodeType.kStreamIn: StreamIn,
-  NodeType.kStreamOut: StreamOut,
-  NodeType.kOpticalStimulation: OpticalStimulation
+    NodeType.kBroadbandSource: BroadbandSource,
+    NodeType.kSpectralFilter: SpectralFilter,
+    NodeType.kSpikeSource: SpikeSource,
+    NodeType.kOpticalStimulation: OpticalStimulation,
 }
 
 SIMULATOR_PERIPHERALS = []
 
 SIMULATOR_DEFAULTS = ENTRY_DEFAULTS.copy()
-SIMULATOR_DEFAULTS["device_serial"] =  "SFI-SIM001"
+SIMULATOR_DEFAULTS["device_serial"] = "SFI-SIM001"
+
 
 def main():
-  server(SIMULATOR_NODE_OBJECT_MAP, SIMULATOR_PERIPHERALS, ENTRY_DEFAULTS)
+    server(SIMULATOR_NODE_OBJECT_MAP, SIMULATOR_PERIPHERALS, ENTRY_DEFAULTS)
