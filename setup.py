7cbb2c9bc5036dd7e61ece92b7c65539e43c0d1c
Calvin Leng <calvinl@science.xyz>
2025-08-22 15:58:20 -0700

    Chore: bump version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ extensions = [
 
 setup(
     name="science-synapse",
-    version="2.3.1",
+    version="2.3.2",
     description="Client library and CLI for the Synapse API",
     author="Science Team",
     author_email="team@science.xyz",
