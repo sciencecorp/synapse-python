837965a595a20edcac706c2272f4e28782023f36
calvinleng-science <calvinl@corporationscience.com>
2026-04-09 15:05:27 -0700

    bump version to 2.6.0a2 for fixed onnxruntime packaging and updated README

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="science-synapse",
-    version="2.6.0a1",
+    version="2.6.0a2",
     description="Client library and CLI for the Synapse API",
     author="Science Team",
     author_email="team@science.xyz",
